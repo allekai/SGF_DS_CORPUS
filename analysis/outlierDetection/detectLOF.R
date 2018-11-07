58e5f80ac9ca66e7e2aef2860ad8fccc99e792ab
Alexander Poth <uneft@student.kit.edu>
2018-11-07 03:11:38 +0100

    Add DBSCAN :)

--- analysis/outlierDetection/detectLOF.R
+++ analysis/outlierDetection/detectLOF.R
@@ -26,6 +26,7 @@
 # The actual code
 list.of.packages  <- c("Rlof",
                        "rlist",
+                       "dbscan",
                        "ROCR",
                        "streamgenerator")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -59,44 +60,49 @@ getStaticStream <- function(n=1000,
 
 
 
-#### Benchmark LOF ####
 # Setup
-max.k <- 10
-stream <- getStaticStream()
-output.stream(stream, "data/testStaticLinear")
-data <- stream$data[-ncol(stream$data)]
-data.labels <- stream$data[ncol(stream$data)]
+max.k <- 20
+prop = 0.01
+n = 10000
+path.data <- "data/testStaticLinear"
+if (!list.files(path="data/", pattern="testStaticLinear_data.txt") > 1) {
+    stream <- getStaticStream(n=n,prop=prop)
+    output.stream(stream, path.data)
+    data <- stream$data[-ncol(stream$data)]
+    data.labels <- stream$data[ncol(stream$data)]
+} else {
+    stream <- read.csv(paste(path.data, "_data.txt", collapse="", sep=""), stringsAsFactors=FALSE)
+    data <- stream[-ncol(stream)]
+    data.labels <- stream[ncol(stream)]
+}
 names(data.labels) <- "label"
 
-# Generate LOFs
-data.lof <- lof(data, c(1:max.k), 2)
-data.lof.labeled <- data.frame(data.lof, data.labels)
+min.Pts <- 3
+max.Pts <- 10
+eps <- 0.5
 
-# Make predictions by setting values to 0 and 1 accordingly
-# If LOF > threshold, make 1 (=outlier) else 0.
-makePredictions.LOF <- function(threshold, lofs) {
-    lofs[lofs < threshold] <- 0
-    lofs[lofs  != 0] <- 1
-    lofs
-}
-for (k in 1:max.k) {
-    varname.pred <- paste("lof.prediction.k.",k,collapse="",sep="")
+# DBSCAN
+for (pts in min.Pts:max.Pts) {
+    varname.pred <- paste("dbscan.prediction.pts.",pts,collapse="",sep="")
     assign(varname.pred, list())
-    varname.perf <- paste("lof.perf.k.",k,collapse="",sep="")
+    varname.perf <- paste("dbscan.perf.pts.",pts,collapse="",sep="")
     assign(varname.perf, list())
-    for (thresh in (seq(from=1,to=2,by=0.1))) {
-        predicted.data <- makePredictions.LOF(threshold=thresh,lofs=data.lof[,k])
-        pred <- prediction(predicted.data, data.labels)
-        assign(varname.pred, list.append(get(varname.pred), pred))
-        perf <- performance(pred, "tpr", "fpr")
-        assign(varname.perf, list.append(get(varname.perf), perf))
-    }
+    predicted.data <- dbscan(data, eps=eps, minPts=pts)
+#    browser()
+    predicted.outlier.indices <- which(predicted.data$cluster == 0)
+    dbscan.outlier.prediction <- rep(0, nrow(data))
+    dbscan.outlier.prediction[predicted.outlier.indices] <- 1
+    pred <- prediction(dbscan.outlier.prediction, data.labels)
+    assign(varname.pred, list.append(get(varname.pred), pred))
+    perf <- performance(pred, "tpr", "fpr")
+    assign(varname.perf, list.append(get(varname.perf), perf))
 }
 
-for (k in 1:max.k) {
-    varname.perf <- paste("lof.perf.k.",k,collapse="",sep="")
-    varname.points <- paste("points.k.",k,collapse="",sep="")
-    varname.points.plottable <- paste("plottable.points.k.",k,collapse="",sep="")
+varname.points.plottable <- paste("plottable.points",collapse="",sep="")
+assign(varname.points.plottable, list())
+for (k in min.Pts:max.Pts) {
+    varname.perf <- paste("dbscan.perf.pts.",k,collapse="",sep="")
+    varname.points <- paste("points.pts.",k,collapse="",sep="")
     assign(varname.points, list())
     for (i in (1:(length(get(varname.perf))))) {
         assign(varname.points,
@@ -104,8 +110,9 @@ for (k in 1:max.k) {
                            c(x=get(varname.perf)[[i]]@x.values[[1]][2],
                              y=get(varname.perf)[[i]]@y.values[[1]][2])))
     }
-    assign(varname.points.plottable,
-           list(x = unlist(lapply(X=get(varname.points),
+    assign(varname.points.plottable, 
+           list.append(get(varname.points.plottable),
+           c(x = unlist(lapply(X=get(varname.points),
                                   FUN=function(x){
                                       res <- x["x"];
                                       names(res) <- NULL;
@@ -114,9 +121,70 @@ for (k in 1:max.k) {
                                   FUN=function(x){
                                       res <- x["y"];
                                       names(res) <- NULL;
-                                      res}))))
+                                      res})))))
 }
+x <- unlist(lapply(get(varname.points.plottable), function(x) x["x"]))
+names(x) <- NULL
+y <- unlist(lapply(get(varname.points.plottable), function(x) x["y"]))
+names(y) <- NULL
+assign(varname.points.plottable, list(x=x, y=y))
 
+# Generate LOFs
+#data.lof <- lof(data, c(1:max.k), 2)
+#data.lof.labeled <- data.frame(data.lof, data.labels)
+#
+## Set the proportion of points specified by outlierProportion to
+## 1 (=outlier) others 0. Take the outlierProportion largest LOFs for
+## this.
+#makePredictions.LOF <- function(lofs, outlierProportion) {
+#    outlier.indices <- order(lofs, decreasing=TRUE)[1:outlierProportion]
+#    lofs <- rep(0, length(lofs))
+#    lofs[outlier.indices] <- 1
+#    lofs
+#}
+#for (k in 1:max.k) {
+#    varname.pred <- paste("lof.prediction.k.",k,collapse="",sep="")
+#    assign(varname.pred, list())
+#    varname.perf <- paste("lof.perf.k.",k,collapse="",sep="")
+#    assign(varname.perf, list())
+#    predicted.data <- makePredictions.LOF(lofs=data.lof[,k], outlierProportion=prop*nrow(data))
+#    pred <- prediction(predicted.data, data.labels)
+#    assign(varname.pred, list.append(get(varname.pred), pred))
+#    perf <- performance(pred, "tpr", "fpr")
+#    assign(varname.perf, list.append(get(varname.perf), perf))
+#}
+#
+#varname.points.plottable <- paste("plottable.points",collapse="",sep="")
+#assign(varname.points.plottable, list())
+#for (k in 1:max.k) {
+#    varname.perf <- paste("lof.perf.k.",k,collapse="",sep="")
+#    varname.points <- paste("points.k.",k,collapse="",sep="")
+#    assign(varname.points, list())
+#    for (i in (1:(length(get(varname.perf))))) {
+#        assign(varname.points,
+#               list.append(get(varname.points),
+#                           c(x=get(varname.perf)[[i]]@x.values[[1]][2],
+#                             y=get(varname.perf)[[i]]@y.values[[1]][2])))
+#    }
+#    assign(varname.points.plottable, 
+#           list.append(get(varname.points.plottable),
+#           c(x = unlist(lapply(X=get(varname.points),
+#                                  FUN=function(x){
+#                                      res <- x["x"];
+#                                      names(res) <- NULL;
+#                                      res})),
+#                y = unlist(lapply(X=get(varname.points),
+#                                  FUN=function(x){
+#                                      res <- x["y"];
+#                                      names(res) <- NULL;
+#                                      res})))))
+#}
+#x <- unlist(lapply(get(varname.points.plottable), function(x) x["x"]))
+#names(x) <- NULL
+#y <- unlist(lapply(get(varname.points.plottable), function(x) x["y"]))
+#names(y) <- NULL
+#assign(varname.points.plottable, list(x=x, y=y))
+#
 #pred.list <- lapply(seq(from=1, to=2, by=0.1),
 #                    function(x) makePredictions.LOF(threshold=x, lofs=data.lof))
 #
---
+++ analysis/outlierDetection/makeExperimentDBSCAN_StaticWall.R
@@ -0,0 +1,141 @@
+list.of.packages  <- c("ggplot2",
+                       "dbscan",
+                       "streamgenerator")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+if(!onlyPlot) {
+prop = 0.01
+n = 10000
+path.data <- "data/testStaticWall"
+
+getStaticStream <- function(n=1000,
+                            dep="Linear",
+                            dim=10,
+                            subDim=3,
+                            margin=0.9,
+                            prop=0.05) {
+    conf <- streamgenerator::generate.stream.config(dim=dim,
+                                                    dependency=dep,
+                                                    nstep=1)
+    conf$subspaces <- list(1:subDim)
+    conf$margins <- margin
+    stream <- streamgenerator::generate.static.stream(n=n,
+                                                      stream.config=conf,
+                                                      prop=prop,
+                                                      method="Construction")
+    stream
+}
+
+stream <- read.csv(paste(path.data,
+                         "_data.txt",
+                         collapse="", sep=""),
+                   stringsAsFactors=FALSE)
+data <- stream[-ncol(stream)]
+data.labels <- stream[ncol(stream)]
+names(data.labels) <- "label"
+
+normalizeDBSCAN.Clusters <- function(clusters) {
+   indices <- which(clusters == 0)
+   outlier.score <- rep(0, length(clusters))
+   outlier.score[indices] <- 1
+   outlier.score
+}
+
+
+getPrecision <- function(predicted.outlier, labels) {
+    # precision = tp /tp + fp
+    predicted.outlier.indices <- which(predicted.outlier == 1)
+    labels.outlier.indices <- which(labels == 1)
+    tp <- length(intersect(predicted.outlier.indices, labels.outlier.indices))
+    fp <- length(setdiff(predicted.outlier.indices, labels.outlier.indices))
+    precision <- tp / (tp + fp)
+    precision
+}
+
+getAccuracy <- function(predicted.outlier, labels) {
+    # accuracy = (tp + tn) / (N)
+    predicted.outlier.indices <- which(predicted.outlier == 1)
+    predicted.inlier.indices <- which(predicted.outlier == 0)
+    labels.outlier.indices <- which(labels == 1)
+    labels.inlier.indices <- which(labels == 0)
+    tp <- length(intersect(predicted.outlier.indices, labels.outlier.indices))
+    tn <- length(intersect(predicted.inlier.indices, labels.inlier.indices))
+    fp <- length(setdiff(predicted.outlier.indices, labels.outlier.indices))
+    fn <- length(setdiff(predicted.inlier.indices, labels.inlier.indices))
+    acc <- (tp + tn) / (tp + tn + fp + fn)
+    acc
+}
+
+getRecall <- function(predicted.outlier, labels) {
+    # recall = tp / (tp + fn)
+    predicted.outlier.indices <- which(predicted.outlier == 1)
+    predicted.inlier.indices <- which(predicted.outlier == 0)
+    labels.outlier.indices <- which(labels == 1)
+    labels.inlier.indices <- which(labels == 0)
+    tp <- length(intersect(predicted.outlier.indices, labels.outlier.indices))
+    tn <- length(intersect(predicted.inlier.indices, labels.inlier.indices))
+    fp <- length(setdiff(predicted.outlier.indices, labels.outlier.indices))
+    fn <- length(setdiff(predicted.inlier.indices, labels.inlier.indices))
+    rec <- tp / (tp + fn)
+    rec
+}
+
+getF1Score <- function(prec, rec) {
+    2 * (prec * rec) / (prec + rec)
+}
+
+filepath = "dbscanresults_WALL.csv"
+eps.steps <- seq(0.1, 0.8, 0.01)
+write('"minPts","eps","Precision", "Recall","F1"', file=filepath)
+for (pts in 2:10) {
+    for (eps in eps.steps) {
+        scan <- dbscan(data, eps=eps, minPts=pts)
+        scan.outlier.prediction <- normalizeDBSCAN.Clusters(scan$cluster)
+
+        prec <- getPrecision(scan.outlier.prediction, data.labels)
+        acc <- getAccuracy(scan.outlier.prediction, data.labels)
+        rec <- getRecall(scan.outlier.prediction, data.labels)
+        f1 <- getF1Score(prec, rec)
+
+        print(paste("DBSCAN Result for eps = ", eps,
+                    " and minPts = ", pts,
+                    "    Precision = ", prec,
+                    "    Recall = ", rec,
+                    "    F1 = ", f1,
+                    collapse="", sep=""))
+        write(paste(pts, eps, prec, rec, f1, collapse="", sep=","),
+              file=filepath, append=TRUE)
+    }
+    df <- read.csv(filepath)
+}
+
+exp.res <- read.csv(filepath)
+f1.scores <- vector(mode="integer")
+for (i in 1:length(2:10)){
+    f1.values <- exp.res[((i-1)*length(eps.steps)+1):(i*length(eps.steps)),"F1"]
+    f1.val <- max(f1.values)
+    f1.scores <- c(f1.scores, f1.val)
+}
+write.table(x=exp.res[which(exp.res$F1 %in% f1.scores),],file="DBSCAN_F1_Scores_Wall.csv",
+            sep=",")
+
+
+
+}
+plot <- ggplot(exp.res, aes(x=Recall, y=Precision)) +
+            geom_line() +
+            theme(plot.title = element_text(hjust = 0.5),
+                  panel.spacing = unit(1, "lines")) +
+            ggtitle("DBSCAN with varying minPts parameter") +
+            facet_wrap(~minPts)
+ggsave(filename="dbscanPrecRecPlotWall.pdf", plot=plot, device="pdf",
+       height = 15,
+       width = 20,
+       units = "cm")
+
+
+
+
+
